90b1ae984c6face873fedba49ffb723326b424ad
dedmen <dedmen@dedmen.de>
2017-11-11 15:31:09 +0100

    Added diagBit setter func

--- interceptTest/diagStuff.cpp
+++ interceptTest/diagStuff.cpp
@@ -152,14 +152,232 @@ game_value drawMode(game_value mode) {
 
 
 
+enum class diagBits {
+    CostMap, //0x0010 first bit
+    LockMap, //0x0011 
+    Combat, //0x0012 
+    Force, //0x0013 
+    Animation, //0x0014 works
+    Damage, //0x0015 
+    Collision, //0x0016 
+    Transparent, //0x0017 
+    Sound, //0x0018 
+    SoundControllers, //0x0019 
+    HDR, //0x001A 
+    Path, //0x001B 
+    Network, //0x001C 
+    ManCollision, //0x001D 
+    Wind, //0x001E 
+    Rumble, //0x001F 
+    AutoAction, //0x0020 
+    Model, //0x0021 works
+    MapScale, //0x0022 
+    ID, //0x0023 
+    Ambient, //0x0024 
+    Updates, //0x0025 
+    EAX, //0x0026 
+    MatLOD, //0x0027 
+    Roads, //0x0028 
+    Light, //0x0029 
+    ContactJoints, //0x002A 
+    PermanentJoints, //0x002B 
+    BBTree, //0x002C 
+    Phys, //0x002D 
+    Resource, //0x002E works
+    FSM, //0x002F works
+    Prune, //0x0030 
+    LogAnimPaths, //0x0031 
+    Streaming, //0x0032 
+    MouseSensitivity, //0x0033 
+    UIControls, //0x0034 works
+    CursorTargetObject, //0x0035 works
+    Terrain, //0x0036 
+    Temperature, //0x0037 Temperature
+    TIHistogram, //0x0038 Seems implemented
+    PathFind, //0x0039 
+    MinObjectDistance, //0x003A 
+    OperCache, //0x003B 
+    ShowGCollision, //0x003C 
+    ShowGRoadway, //0x003D 
+    LateInit, //0x003E 
+    Convoy, //0x003F 
+    ProfileScripts, //0x0040 
+    CombatMode, //0x0041 
+    EPE, //0x0042 works
+    EPEDraw, //0x0043 
+    Shots, //0x0044 
+    Impact, //0x0045 
+    Radius, //0x0046 
+    Speed, //0x0047 
+    Mines, //0x0048 
+    MinesDetect, //0x0049 
+    Anomalies, //0x004A 
+    Fatigue, //0x004B 
+    Complexity, //0x004C workd
+    pruneX, //0x004D 
+    Scene, //0x004E 
+    Weather, //0x004F 
+    LogicalWater, //0x0050 
+    CameraTransform, //0x0051 
+    AmbLife, //0x0052 
+    DamageSimulator, //0x0053 
+    DEHitPoints, //0x0054 
+    DEHitPointsText, //0x0055 
+    SoldierTransform, //0x0056 
+    Buoyancy, //0x0057 
+    WaterSplash, //0x0058 
+    Effects, //0x0059 
+    SimulWeather, //0x005A 
+    ParticleGen, //0x005B 
+    Visibility, //0x005C 
+    EPESimul, //0x005D 
+    BBox, //0x005E 
+    AnimSrcUnit, //0x005F 
+    AnimSrcTarget, //0x0060 
+    VisualStates, //0x0061 Lags the game.. So probably does something
+    VisualStatesIcon, //0x0062 
+    VisualStatesWeapon, //0x0063 
+    Joystick, //0x0064 
+    Gearbox, //0x0065 
+    Impulse, //0x0066 
+    Destruct, //0x0067 
+    MoveForces, //0x0068 
+    ObjMatrix, //0x0069 
+    PXMatrix, //0x006A 
+    Suspension, //0x006B 
+    EPEPerf, //0x006C 
+    EPEStats, //0x006D 
+    EPEForce, //0x006E 
+    EPEVehicle, //0x006F 
+    Formations, //0x0070 works. But looks like it's waypoint
+    Waypoints, //0x0071 
+    ParticleNames, //0x0072 works
+    WingVortices, //0x0073 
+    FootSteps, //0x0074 works 
+    Keyboard, //0x0075 
+    Particles, //0x0076 
+    ObjNames, //0x0077 
+    ObjIds, //0x0078 
+    PXCollision, //0x0079 
+    SoundMap, //0x007A 
+    DynLights, //0x007B 
+    Head, //0x007C 
+    Clutter, //0x007D 
+    LightGrid, //0x007E 
+    RagDoll, //0x007F 
+    RagDollMP, //0x0080 
+    Attributes, //0x0081 
+    Locality, //0x0082 
+    PP, //0x0083 
+    RestrictedZones, //0x0084 
+    WaterObjects, //0x0085 
+    Impulses, //0x0086 
+    ProtocolExpressions, //0x0087 
+    DrawCOM, //0x0088 
+    BodyRemove, //0x0089 
+    WreckRemove, //0x008A 
+    Aiming, //0x008B 
+    viewDensity, //0x008C 
+    Exhaust, //0x008D 
+    Throwing, //0x008E 
+    ModelLights, //0x008F 
+    DestructEffects, //0x0090 
+    UnitStats, //0x0091 
+    Land, //0x0092 
+    AnimationCode, //0x0093 
+    Camera, //0x0094 
+    InvisibleVehicle, //0x0095 
+    WorldObjects, //0x0096 sound scene stuff
+    WeaponResting, //0x0097 
+    Matrices, //0x0098 
+    Zoom, //0x0099 
+    PerfInfo, //0x009A 
+    ObjPassCount, //0x009B works
+    RotAxisOffset, //0x009C 
+    ParticleVis, //0x009D 
+    ParticleVisJobs, //0x009E 
+    Rain, //0x009F 
+    DlcContent, //0x00A0 
+    DlcChecks, //0x00A1 
+    StatsManager, //0x00A2 
+    StatsAchievements, //0x00A3 
+    StatsGlobal, //0x00A4 
+    Supply, //0x00A5 
+    WeaponSway, //0x00A6 
+    WeaponMatrices, //0x00A7 
+    ParticlesInput, //0x00A8 
+    DlcUsage, //0x00A9 
+    DlcPopup, //0x00AA 
+    SurfaceUnder, //0x00AB Works
+    Turret, //0x00AC 
+    Ropes, //0x00AD 
+    WeaponDistortion, //0x00AE 
+    Recoil, //0x00AF 
+    Breathing, //0x00B0 
+    Suppress, //0x00B1 
+    SuppressMap, //0x00B2 
+    FFVInertia, //0x00B3 
+    FFVObstruction, //0x00B4 
+    AttachedOn, //0x00B5 
+    CharRotations, //0x00B6 
+    WeaponDeployment, //0x00B7 
+    CameraShake, //0x00B8 
+    FriendsManager, //0x00B9 
+    SoundBullet, //0x00BA  Super sonic crack sources
+    LeaderboardsManager, //0x00BB 
+    AISkill, //0x00BC 
+    AIDriving, //0x00BD 
+    AISteering, //0x00BE 
+    AIAiming, //0x00BF 
+    AIGroup, //0x00C0 
+    AIBrain, //0x00C1 
+    AIDynamicError, //0x00C2 
+    AISuppression, //0x00C3 
+    AICover, //0x00C4 
+    AIAvoidCollision, //0x00C5 
+    AIConvoy, //0x00C6 
+    AIWeapon, //0x00C7 
+    InteractiveAreas3DEN, //0x00C8 
+    RenderLODs, //0x00C9 
+    Analytics, //0x00CA 
+    ObjectListCells, //0x00CB 
+    Performance, //0x00CC 
+    VehicleTransport, //0x00CD 
+    LightSimple, //0x00CE 
+    MobileWeaponSwitch, //0x00CF 
+    SlopeCollision, //0x00D0 
+    SoundEnv, //0x00D1 
+    SensorsVehicle, //0x00D2 works
+    SensorsAmmo, //0x00D3 
+    DynSimGrid, //0x00D4  works
+    DynSimEntities, //0x00D5  works
+    DynSimGroups, //0x00D6 works
+    last
+};
+struct diagbitsS {  //&arma3_x64.exe + 0x2707400
+    char arr[static_cast<int>(diagBits::last)];
+};
+#include <windows.h>
+#include <Psapi.h>
+#pragma comment (lib, "Psapi.lib")//GetModuleInformation
+
+game_value diagBit(game_value mode) {
+    float m = mode;
+    int offs = (int) m;
+    MODULEINFO modInfo = { 0 };
+    HMODULE hModule = GetModuleHandle(NULL);
+    GetModuleInformation(GetCurrentProcess(), hModule, &modInfo, sizeof(MODULEINFO));
+    auto engineBase = reinterpret_cast<uintptr_t>(modInfo.lpBaseOfDll);
+    diagbitsS* dBits = reinterpret_cast<diagbitsS*>(engineBase + 0x2707400 +0x10) ;
+    dBits->arr[offs] = !dBits->arr[offs];
+    return {};
+}
+
 
 
 uintptr_t placeHookTotalOffs(uintptr_t totalOffset, uintptr_t jmpTo);
 
 
-#include <windows.h>
-#include <Psapi.h>
-#pragma comment (lib, "Psapi.lib")//GetModuleInformation
 
 void diagStuff::preStart() {
 
@@ -177,11 +395,9 @@ void diagStuff::preStart() {
 
 
     static auto mfunc = intercept::client::host::registerFunction("setDrawMode", "", userFunctionWrapper<drawMode>, GameDataType::NOTHING, GameDataType::STRING);
+    static auto mfunc2 = intercept::client::host::registerFunction("setDiagMode", "", userFunctionWrapper<diagBit>, GameDataType::NOTHING, GameDataType::SCALAR);
 
 }
-
-
-
 
 class diagbits {  //&arma3_x64.exe + 0x2707400
 public:
